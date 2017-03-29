cdf1179dbb2940a0977cf4e640afededb8211f01
Mohammed Ismail <ikismail7@gmail.com>
2017-03-29 22:01:51 +0530

    Added  various Theme to UI

---
+++ js/themeChanger.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ikism on 29-Mar-17.
+ */
+
+$('#blue').click(function (){
+   document.getElementById('styleChanger').href = "css/blue.css";
+});
+$('#rose').click(function (){
+    document.getElementById('styleChanger').href = "css/rose.css";
+});
+$('#home').click(function (){
+    document.getElementById('styleChanger').href = "css/style.css";
+});
